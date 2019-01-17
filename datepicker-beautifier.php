4d9d03c84d0d472c0dd2e4c5e0334107311a197d
zain <zainulabd786@gmail.com>
2019-01-17 23:23:44 +0530

    Updates Code

--- datepicker-beautifier.php
+++ datepicker-beautifier.php
@@ -16,6 +16,7 @@ add_action('wp_enqueue_scripts', 'dpb_enqueue_script');
 
 
 function dpb_datepicker_styles($args){ ?>
+	<div class="dpb-styles">
 	<style>
 		.ui-datepicker select {
 			-webkit-appearance: none!important;
@@ -33,9 +34,6 @@ function dpb_datepicker_styles($args){ ?>
 		}
 		.ui-datepicker-header{
 			background-color: <?= $args['headerColor'] ?>
-		}
-		.ui-datepicker-prev span{
-			color: #fff!important;
 		}
 		.ui-datepicker-next, .ui-datepicker-prev{
 			line-height: 300%!important;
@@ -96,84 +94,285 @@ function dpb_datepicker_styles($args){ ?>
 		.ui-datepicker-prev-year{
 			float: left;
 		}
-	</style><?php
+	</style>
+	</div><?php
 }
-//add_action("wp_footer", "dpb_datepicker_styles");
+add_action("wp_footer", "dpb_datepicker_styles");
 
 
-
-
-
-
-
-/*function my_function($placement, $form_id) {
-    if( $placement == 500 ) {
-		$form = GFAPI::get_form( $form_id ); ?>
-		<script type="text/javascript">
-			jQuery(document).on('gform_load_field_settings', function(event, field) {
-				// custom JS code here
-				if(field.type == "date"){
-					
-				}
-			});
-		</script><?php
-    }
+add_action("wp_ajax_get_field_object", "get_field_object");
+add_action("wp_ajax_nopriv_get_field_object", "get_field_object");
+function get_field_object(){
+	$form = GFAPI::get_form( $_POST['formID'] );
+	echo json_encode($form);
+	//echo "<pre>"; print_r($form); echo "</pre>";
+	wp_die();
 }
-add_action( 'gform_field_appearance_settings', 'my_function', 10, 2 );*/
-
-
-
-add_filter( 'gform_field_content', 'dbp_render_styles', 10, 5 );
-function dbp_render_styles( $field_content, $field ){
-	//echo "<pre>"; print_r($field); echo "</pre>";
-	if ( $field->type == 'date' ) {
-		$headerColor = $field->headerColor;
-		//dpb_datepicker_styles(array('headerColor' => $headerColor));
-		$field = json_encode($field);
-	}
-	return $field_content;
-}
-
-
 
 add_action( 'gperk_field_settings', 'dpb_calendar_appearance' ) ;
 function dpb_calendar_appearance() {
     ?>
-
-    <li class="dpb-calendar-appearance-setting field_setting">
-
-        <input type="color" id="dpbca-header-color" onchange="SetFieldProperty( 'headerColor', this.value);">
-
-        <label class="inline" for="dpbca-header-color">
+	<h3>
+		Datepicker Settings	
+	</h3>
+	<li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-calendar-size">
             <?php 
-				_e( 'Header Color' );
-            	 gform_tooltip( 'dpbca-header-color-toolip' ); 
+				_e( 'Calendar Size' );
 			?>
         </label>
-
+        <select id="dpbca-calendar-size"  onchange="SetFieldProperty( 'calendarSize', this.value);">
+			<option value="sm" selected>Small</option>
+			<option value="md">Medium</option>
+			<option value="lg">large</option>
+		</select>
+    </li>
+	<h4>
+		Header
+	</h4>
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-header-color">
+            <?php 
+				_e( 'Header Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-header-color"  onchange="SetFieldProperty( 'headerColor', this.value);">
     </li>
 
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-header-font-size">
+            <?php 
+				_e( 'Font Size' );
+			?>
+        </label>
+        <input type="text" id="dpbca-header-font-size" onchange="SetFieldProperty( 'headerFontSize', this.value);" placeholder="in pixels">
+    </li>
+
+	<li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-header-arrows-color">
+            <?php 
+				_e( 'Header Arrows Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-header-arrows-color" onchange="SetFieldProperty( 'headerArrowsColor', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-header-padding">
+            <?php 
+				_e( 'Header Padding' );
+			?>
+        </label>
+        <input type="text" id="dpbca-header-padding" onchange="SetFieldProperty( 'headerPadding', this.value);">
+    </li>
+    
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-header-arrow-size">
+            <?php 
+				_e( 'Header Arrow Size' );
+			?>
+        </label>
+        <input type="text" id="dpbca-header-arrow-size" onchange="SetFieldProperty( 'headerArrowSize', this.value);">
+    </li>
+    <h4>
+		Week Strip
+	</h4>
+	<li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-weeks-strip-color">
+            <?php 
+				_e( 'Weeks Strip Background' );
+			?>
+        </label>
+        <input type="color" id="dpbca-weeks-strip-color" onchange="SetFieldProperty( 'weekStripColor', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-week-strip-font-size">
+            <?php 
+				_e( 'Week Font Size' );
+			?>
+        </label>
+        <input type="text" id="dpbca-week-strip-font-size" onchange="SetFieldProperty( 'weekStripFontSize', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-week-strip-font-color">
+            <?php 
+				_e( 'Week Font color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-week-strip-font-color" onchange="SetFieldProperty( 'weekStripFontColor', this.value);">
+    </li>
+	<h4>
+		Calendar Body
+	</h4>
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-date-font-size">
+            <?php 
+				_e( 'Date Font Size' );
+			?>
+        </label>
+        <input type="text" id="dpbca-date-font-size" onchange="SetFieldProperty( 'dateFontSize', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-date-font-color">
+            <?php 
+				_e( 'Date Font Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-date-font-color" onchange="SetFieldProperty( 'dateFontcolor', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-date-padding">
+            <?php 
+				_e( 'Date Padding' );
+			?>
+        </label>
+        <input type="text" id="dpbca-date-padding" onchange="SetFieldProperty( 'datePadding', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-date-background-color">
+            <?php 
+				_e( 'Date Background Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-date-background-color" onchange="SetFieldProperty( 'dateBackgroundcolor', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-highlighted-date-font-size">
+            <?php 
+				_e( 'Highlated Date Font Size' );
+			?>
+        </label>
+        <input type="text" id="dpbca-highlighted-date-font-size" onchange="SetFieldProperty( 'highlatedDateFontSize', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-highlighted-date-font-color">
+            <?php 
+				_e( 'Highlated Date Font Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-highlighted-date-font-color" onchange="SetFieldProperty( 'highlatedDateFontColor', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-highlighted-date-background-color">
+            <?php 
+				_e( 'Highlated Date Background Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-highlighted-date-background-color" onchange="SetFieldProperty( 'highlatedDateBackgroundColor', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-highlighted-date-border-radius">
+            <?php 
+				_e( 'Highlated Date Border Radius' );
+			?>
+        </label>
+        <input type="text" id="dpbca-highlighted-date-border-radius" onchange="SetFieldProperty( 'highlatedDateBorderRadius', this.value);">
+    </li>
+	<h4>
+		Footer
+	</h4>
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-footer-background-color">
+            <?php 
+				_e( 'Footer Background Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-footer-background-color" onchange="SetFieldProperty( 'footerBackgroundColor', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-footer-font-color">
+            <?php 
+				_e( 'Footer Font Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-footer-font-color" onchange="SetFieldProperty( 'footerFontColor', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-footer-arrow-color">
+            <?php 
+				_e( 'Footer Arrow Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-footer-arrow-color" onchange="SetFieldProperty( 'footerArrowColor', this.value);">
+    </li>
+
+     <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-footer-arrow-size">
+            <?php 
+				_e( 'Footer Arrow Size' );
+			?>
+        </label>
+        <input type="text" id="dpbca-footer-arrow-size" onchange="SetFieldProperty( 'footerArrowSize', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-footer-font-size">
+            <?php 
+				_e( 'Footer Font Size' );
+			?>
+        </label>
+        <input type="text" id="dpbca-footer-font-size" onchange="SetFieldProperty( 'footerFontSize', this.value);">
+    </li>
+
+    <li class="dpb-calendar-appearance-setting field_setting">
+        <label for="dpbca-disabled-date-color">
+            <?php 
+				_e( 'Disabled Date Color' );
+			?>
+        </label>
+        <input type="color" id="dpbca-disabled-date-color" onchange="SetFieldProperty( 'disableDateColor', this.value);">
+    </li>
+    
     <?php
 }
 
 add_action( 'gform_editor_js', 'field_settings_js' );
-function field_settings_js() {
-    ?>
-
-<script type="text/javascript">
-    (function($) {
-        $(document).bind( 'gform_load_field_settings', function( event, field, form ) {
-            // populates the stored value from the field back into the setting when the field settings are loaded
-            $( '#dpbca-header-color' ).val(field['headerColor']);
-            // if our desired condition is met, we show the field setting; otherwise, hide it
-            if( GetInputType( field ) == 'date' ) {
-                $( '.dpb-calendar-appearance-setting' ).show();
-            } else {
-                $( '.dpb-calendar-appearance-setting' ).hide();
-            }
-        } );
-    })(jQuery);
-</script>
-
-<?php
+function field_settings_js() { ?>
+	<script type="text/javascript">
+	    (function($) {
+	        $(document).bind( 'gform_load_field_settings', function( event, field, form ) {
+	            // populates the stored value from the field back into the setting when the field settings are loaded
+	            $( '#dpbca-calendar-size' ).val(field['calendarSize']);
+	            $( '#dpbca-header-color' ).val(field['headerColor']);
+				$( '#dpbca-header-font-size' ).val(field['headerFontSize']);
+				$( '#dpbca-header-arrows-color' ).val(field['headerArrowsColor']);
+				$( '#dpbca-header-padding' ).val(field['headerPadding']);
+				$( '#dpbca-header-arrow-size' ).val(field['headerArrowSize']);
+				$( '#dpbca-weeks-strip-color' ).val(field['weekStripColor']);
+				$( '#dpbca-week-strip-font-size' ).val(field['weekStripFontSize']);
+				$( '#dpbca-week-strip-font-color' ).val(field['weekStripFontColor']);
+				$( '#dpbca-date-font-size' ).val(field['dateFontSize']);
+				$( '#dpbca-date-font-color' ).val(field['dateFontColor']);
+				$( '#dpbca-date-padding' ).val(field['dateDadding']);
+				$( '#dpbca-date-background-color' ).val(field['dateBackgroundColor']);
+				$( '#dpbca-highlighted-date-font-size' ).val(field['highlatedDateFontSize']);
+				$( '#dpbca-highlighted-date-font-color' ).val(field['highlatedDateFontColor']);
+				$( '#dpbca-highlighted-date-background-color' ).val(field['highlatedDateBackgroundColor']);
+				$( '#dpbca-highlighted-date-border-radius' ).val(field['highlatedDateBorderRadius']);
+				$( '#dpbca-footer-background-color' ).val(field['footerBackgroundColor']);
+				$( '#dpbca-footer-font-color' ).val(field['footerFontColor']);
+				$( '#dpbca-footer-arrow-color' ).val(field['footerArrowColor']);
+				$( '#dpbca-footer-arrow-size' ).val(field['footerArrowSize']);
+				$( '#dpbca-footer-font-size' ).val(field['footerFontSize']);
+				$( '#dpbca-disabled-date-color' ).val(field['disableDateColor']);
+	            // if our desired condition is met, we show the field setting; otherwise, hide it
+	            if( GetInputType( field ) == 'date' ) {
+	                $( '.dpb-calendar-appearance-setting' ).show();
+	            } else {
+	                $( '.dpb-calendar-appearance-setting' ).hide();
+	            }
+	        } );
+	    })(jQuery);
+	</script> <?php
 }
--- js/main.js
+++ js/main.js
@@ -1,27 +1,84 @@
 jQuery(function($){
-	const AJAX_URL = ajax_object.ajax_url;
-	console.log(AJAX_URL);
-	$(".datepicker").on('focus click change',function(){
-		$(".datepicker").is(":focus") && setTimeout(() => dpb_onFocus($), 100);
-	});		
-	$("body").on("click", ".ui-datepicker-next-year", () => {
-		let val = parseInt($(".ui-datepicker-year").val());
-		let nextYear = val+1;
-		($(".ui-datepicker-year").find("option[value='"+nextYear+"']").length) ? ($(".ui-datepicker-year").val(nextYear), $(".ui-datepicker-year").trigger('change')) : $(".ui-datepicker-year").val(val);
-		 
-	});
-	$("body").on("click", ".ui-datepicker-prev-year", () => {
-		let val = parseInt($(".ui-datepicker-year").val());
-		let prevYear = val-1;
-		($(".ui-datepicker-year").find("option[value='"+prevYear+"']").length) ? ($(".ui-datepicker-year").val(prevYear), $(".ui-datepicker-year").trigger('change')) : $(".ui-datepicker-year").val(val);
-		
-	});
-	function dpb_onFocus($){
-		$(".ui-datepicker-prev span").text("<"); /* put "<" in previous month icon */
-		$(".ui-datepicker-next span").text(">"); /* put ">" in next month icon */
-		!$('.ui-datepicker-footer').length && $(".ui-datepicker-calendar").after("<div class='ui-datepicker-footer'></div>");	/* create a div for footer */
-		$(".ui-datepicker-year").appendTo(".ui-datepicker-footer");  /*move year to the footer*/
-		!$('.ui-datepicker-prev-year').length && $(".ui-datepicker-year").before("<span class='ui-datepicker-prev-year'><</span>");
-		!$('.ui-datepicker-next-year').length && $(".ui-datepicker-year").after("<span class='ui-datepicker-next-year'>></span>");
-	}
+    const AJAX_URL = ajax_object.ajax_url;
+    console.log(AJAX_URL);
+    $(".datepicker").on('focus click change',function(){
+        $(".datepicker").is(":focus") && setTimeout(() => dpb_onFocus($), 100);
+    });     
+    $("body").on("click", ".ui-datepicker-next-year", () => {
+        let val = parseInt($(".ui-datepicker-year").val());
+        let nextYear = val+1;
+        ($(".ui-datepicker-year").find("option[value='"+nextYear+"']").length) ? ($(".ui-datepicker-year").val(nextYear), $(".ui-datepicker-year").trigger('change')) : $(".ui-datepicker-year").val(val);
+         
+    });
+    $("body").on("click", ".ui-datepicker-prev-year", () => {
+        let val = parseInt($(".ui-datepicker-year").val());
+        let prevYear = val-1;
+        ($(".ui-datepicker-year").find("option[value='"+prevYear+"']").length) ? ($(".ui-datepicker-year").val(prevYear), $(".ui-datepicker-year").trigger('change')) : $(".ui-datepicker-year").val(val);
+        
+    });
+    
+    jQuery(document).on('gform_post_render', function(event, form_id){
+        
+        $.post(AJAX_URL, {action:"get_field_object", formID:form_id}, (resp) => {
+            let formData = JSON.parse(resp);
+            console.log(formData);
+            $(".datepicker").on('focus click change', function(){
+                let fieldID = parseInt($(this).attr("name").split("_")[1]);
+                formData.fields.forEach( i => {
+                    if(i.id === fieldID){
+                        /****************************** Handle Calendar Size**************************/
+                        i.calendarSize == "sm" == false && (
+                            i.calendarSize == 'md' && (
+                                //$( '.ui-datepicker' ).each(function(){ this.style.setProperty( 'width', '300px', 'important' ) }),
+                                $( '.ui-datepicker' ).css("width", "300px"),
+                                $( '.ui-datepicker td a, .ui-datepicker td span' ).each(function() { this.style.setProperty( 'width', '42px', 'important' ), 
+                                                                                               this.style.setProperty( 'height', '42px', 'important' ) } ),
+                                $( '.ui-datepicker-header' ).each(function(){this.style.setProperty( 'width', "300px", 'important' )}),
+                                $( '.ui-datepicker td a, .ui-datepicker td span' ).each(function(){this.style.setProperty( 'line-height', "42px", 'important' )}),
+                                $( '.ui-datepicker-footer' ).css("width", "300px")
+                            ),
+                            i.calendarSize == 'lg' && (
+                                //$( '.ui-datepicker' ).each(() => this.style.setProperty( 'width', '350px', 'important' )),
+                                $( '.ui-datepicker' ).css("width", "350px"),
+                                $( '.ui-datepicker td a, .ui-datepicker td span' ).each(function() { this.style.setProperty( 'width', '49px', 'important' ); 
+                                                                                               this.style.setProperty( 'height', '49px', 'important' );  } ),
+                                $( '.ui-datepicker-header' ).each(function(){this.style.setProperty( 'width', "350px", 'important' )}),
+                                $( '.ui-datepicker td a, .ui-datepicker td span' ).each(function(){this.style.setProperty( 'line-height', "49px", 'important' )}),
+                                $( '.ui-datepicker-footer' ).css("width", "350px")
+                            )   
+                        );
+                        /****************************** Handle Calendar Size end **************************/
+                        
+                        $( '.ui-datepicker-header' ).each(function(){this.style.setProperty( 'background-color', i.headerColor, 'important' )});
+                        
+                        $( '.ui-datepicker-title .ui-datepicker-month' ).each(function () {
+                            this.style.setProperty( 'background-color', i.headerColor, 'important' );
+                            this.style.setProperty( 'font-size', i.headerFontSize, 'important' )
+                        });
+                        $( '.ui-datepicker-prev, .ui-datepicker-next' ).each(function () {
+                            this.style.setProperty( 'color', i.headerArrowsColor, 'important' );
+                            this.style.setProperty( 'font-size', i.headerArrowSize, 'important' );
+                            this.style.setProperty( 'line-height', 'inherit', 'important' );
+                            this.style.setProperty( 'display', 'flex', 'important' );
+                            this.style.setProperty( 'justify-content', 'center', 'important' );
+                            this.style.setProperty( 'align-items', 'center', 'important' );
+                        });
+                        $('.ui-datepicker-header').each(function () { this.style.setProperty( 'padding', i.headerPadding, 'important' ); });
+                        
+                        $('.ui-datepicker th').each(function () { this.style.setProperty( 'font-size', i.weekStripFontSize, 'important' ); });
+                        $('.ui-datepicker-calendar .ui-state-default').each(function () { this.style.setProperty( 'font-size', i.dateFontSize, 'important' ); });
+                    }
+                })
+            });
+        })
+    });
+    
+    function dpb_onFocus($){
+        $(".ui-datepicker-prev span").text("<"); /* put "<" in previous month icon */
+        $(".ui-datepicker-next span").text(">"); /* put ">" in next month icon */
+        !$('.ui-datepicker-footer').length && $(".ui-datepicker-calendar").after("<div class='ui-datepicker-footer'></div>");   /* create a div for footer */
+        $(".ui-datepicker-year").appendTo(".ui-datepicker-footer");  /*move year to the footer*/
+        !$('.ui-datepicker-prev-year').length && $(".ui-datepicker-year").before("<span class='ui-datepicker-prev-year'><</span>");
+        !$('.ui-datepicker-next-year').length && $(".ui-datepicker-year").after("<span class='ui-datepicker-next-year'>></span>");
+    }
 });
